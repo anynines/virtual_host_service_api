49887c3c650bdbb010681bd32ca7f82635d476fd
Samuel <skretzschmann@anynines.com>
2023-08-02 14:32:52 +0200

    make Honeybadger optional, make secrets configurable through envs

--- config/initializers/honeybadger.rb
+++
@@ -1,5 +0,0 @@
-if ENV['HONEYBADGER_API_KEY'].present?
-  Honeybadger.configure do |config|
-    config.api_key = ENV['HONEYBADGER_API_KEY']
-  end
-end
--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -3,4 +3,11 @@ require 'erubis'
 
 APP_CONFIG = YAML.load_file(Rails.root.join('config/application.yml'))[Rails.env]
 APP_CONFIG['amqp'] = YAML.load(Erubis::Eruby.new(File.read(Rails.root.join('config/amqp.yml'))).result)[Rails.env].symbolize_keys
-  
+
+if APP_CONFIG['honeybadger_api_key'].present?
+  Honeybadger.configure do |config|
+    config.api_key = APP_CONFIG['honeybadger_api_key']
+  end
+end
+
+Rails.application.credentials = APP_CONFIG
